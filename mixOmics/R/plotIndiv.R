3060b442f20e6558fe8bbbbbeaa4f473ac264aeb
Florian Rohart <f,rohart@uq.edu.au>
2014-12-15 10:50:07 +1000

    removed the warnings concerning the arguments. It was useless since the code allows extra arguments to be enter (...)

--- mixOmics/R/plotIndiv.R
+++ mixOmics/R/plotIndiv.R
@@ -312,16 +312,7 @@ plotIndiv.ipca <- plotIndiv.sipca <-
         stop("'ind.names' must be a character vector of length ", nrow(object$x), " or a boolean atomic vector.")
     }
     
-    # check that the user did not enter extra arguments #
-    # --------------------------------------------------#
-    # added by Florian
-    ## what the function is expecting
-    match.function=names(formals(sys.function()))
-    ## what the user has entered
-    match.user = names(match.call())[-1]
-    if(length(setdiff(match.user, match.function)) > 0) warning('Some of the input arguments do not match the function arguments, see ?plotIndiv')
-    
-    
+   
 
     # l'espace de représentation #
     #----------------------------#
@@ -395,16 +386,7 @@ plotIndiv.sgcca <- plotIndiv.rgcca <-
     comp2 = round(comp[2])
     ##rep.space = match.arg(rep.space, c("XY-variate", "X-variate", "Y-variate"))
     
-    # check that the user did not enter extra arguments #
-    # --------------------------------------------------#
-    # added by Florian
-    ## what the function is expecting
-    match.function=names(formals(sys.function()))
-    ## what the user has entered
-    match.user = names(match.call())[-1]
-    if(length(setdiff(match.user, match.function)) > 0) warning('Some of the input arguments do not match the function arguments, see ?plotIndiv')
-    
-    
+   
 
     # l'espace de représentation #
     #----------------------------#
