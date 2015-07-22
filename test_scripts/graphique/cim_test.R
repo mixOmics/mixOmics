a8b6b170ff37c03b9e83c5b626e406e371bfdf75
fbartolo <f.bartolo11@gmail.com>
2015-07-22 12:56:39 +0200

    correction test

--- test_scripts/graphique/cim_test.R
+++ test_scripts/graphique/cim_test.R
@@ -323,10 +323,21 @@ cim(res.2level, sample.sideColors = cbind(stim.col, time.col),
 ## CIM representation for objects of class spls 'multilevel' 
 #------------------------------------------------------------------
 
-# FB est ce que tu peux me rajouter un exemple pr un object de type spls multilevel? Merci!
+data(liver.toxicity)
+repeat.indiv <- c(1, 2, 1, 2, 1, 2, 1, 2, 3, 3, 4, 3, 4, 3, 4, 4, 5, 6, 5, 5,
+                  6, 5, 6, 7, 7, 8, 6, 7, 8, 7, 8, 8, 9, 10, 9, 10, 11, 9, 9,
+                  10, 11, 12, 12, 10, 11, 12, 11, 12, 13, 14, 13, 14, 13, 14,
+                  13, 14, 15, 16, 15, 16, 15, 16, 15, 16)
 
+design <- data.frame(sample = repeat.indiv) 
+res.spls.1level <- multilevel(X = liver.toxicity$gene,
+                              Y=liver.toxicity$clinic,
+                              design = design,
+                              ncomp = 3,
+                              keepX = c(50, 50, 50), keepY = c(5, 5, 5),
+                              method = 'spls', mode = 'canonical')
 
-
-
-
-
+stim.col <- c("darkblue", "purple", "green4","red3")
+cim(res.spls.1level,mapping="Y",
+    sample.sideColors = stim.col[factor(liver.toxicity$treatment[,3])],
+    legend=list(legend=unique(liver.toxicity$treatment[,3]),col=stim.col,cex=0.9))
